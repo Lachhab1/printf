90080167e51216f133f5819b406f3b5154eb4aea
Lachhab1 <Mohamed172lachhab@gmail.com>
2023-04-24 05:46:19 -0700

    unsigned checked

--- specifier.c
+++ specifier.c
@@ -86,7 +86,7 @@ int print_unsigned(va_list list)
 {
 	unsigned int num, n;
 	int t = 1, len = 0;
-
+	
 	n = va_arg(list, unsigned int);
 	num = n;
 	while (num / t > 9)
@@ -98,7 +98,9 @@ int print_unsigned(va_list list)
 		t /= 10;
 	}
 	num = va_arg(list, unsigned int);
+	if (n == 0)
+		return (len);
 	if (n < 1)
 		return (-1);
 	return (len);
-}
+}	
