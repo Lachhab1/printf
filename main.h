72d6691d5fa5688036584c0e633d1b1f6066adba
BADR <benabboudbadr@gmail.com>
2023-04-24 14:56:42 -0700

    tasks

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 /**
-* structt convert - defines a structure for symbols and functions
+* struct convert - defines a structure for symbols and functions
 *
 * @sym: The operator
 * @f: The function associated
