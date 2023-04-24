86b2b92f987393598e7661b700071c9c793fb0d9
Lachhab1 <Mohamed172lachhab@gmail.com>
2023-04-24 03:09:02 -0700

    Attemp 100 to req 5

--- main_test/testString.c
+++ main_test/testString.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	int len, len2;
+	char c = 'K';
 
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	(void) len;
-	(void) len2;
+	len = _printf("Percent:[%%],%c\n",c);
+	len2 = printf("Percent:[%%],%c\n",c);
+	printf("%d\n",len);
+	printf("%d\n",len2);
 	return (0);
 }
